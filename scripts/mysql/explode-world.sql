9e7f6135c83626db6c6d0b17e81f261f15e911e7
Mariano Crowe <merunga@gmail.com>
2015-11-06 09:00:52 -0500

    export file

--- scripts/mysql/explode-world.sql
+++ scripts/mysql/explode-world.sql
@@ -9,19 +9,21 @@ SELECT * FROM (
    "Population" as "Poblacion"
   FROM DUAL
   UNION
-  (SELECT
-   country.Continent as "Continente",
-   country.Name as "Pais",
-   city.District as "Departamento",
-   city.Name as "Ciudad",
-   city.Population as "Poblacion"
-  FROM City as city
-  JOIN Country as country ON city.CountryCode = country.Code
-  ORDER BY
-    country.Name ASC,
-    city.District ASC,
-    city.Name ASC
-  LIMIT 100)
+  (
+    SELECT
+     country.Continent as "Continente",
+     country.Name as "Pais",
+     city.District as "Departamento",
+     city.Name as "Ciudad",
+     city.Population as "Poblacion"
+    FROM City as city
+    JOIN Country as country ON city.CountryCode = country.Code
+    ORDER BY
+      country.Name ASC,
+      city.District ASC,
+      city.Name ASC
+    -- LIMIT 100
+  )
 ) AS data
 INTO OUTFILE "c:\\Users\\merunga\\Documents\\GitHub\\opendata-pe\\output\\ciudades-del-mundo.csv"
 FIELDS TERMINATED BY ','
