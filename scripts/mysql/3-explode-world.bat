44cf5d4df15136dc6100d84870ecbbd64711d5ca
Mariano Crowe <merunga@gmail.com>
2015-11-06 04:38:20 -0500

    quitar soporte a dropbox

--- scripts/mysql/3-explode-world.bat
+++ scripts/mysql/3-explode-world.bat
@@ -1,5 +1,5 @@
 del "..\..\output\ciudades-del-mundo.csv"
 c:\wamp\bin\mysql\mysql5.6.17\bin\mysql.exe -u root -e "SOURCE explode-world.sql"
-copy "..\..\output\ciudades-del-mundo.csv"  "c:\Users\merunga\Google Drive\opendata-pe\"
+:: copy "..\..\output\ciudades-del-mundo.csv"  "c:\Users\merunga\Google Drive\opendata-pe\"
 copy "..\..\output\ciudades-del-mundo.csv"  "c:\Users\merunga\Dropbox\opendata-pe\"
 cmd \k
